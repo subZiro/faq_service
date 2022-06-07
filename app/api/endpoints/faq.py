ffbeb48b390e0c363473960c78c42164e824c46b
Ilya <49365758+subZiro@users.noreply.github.com>
2022-06-07 18:22:03 +0400

    удалены пользователи из роутов с сущностью вопрос-ответ

--- app/api/endpoints/faq.py
+++ app/api/endpoints/faq.py
@@ -2,13 +2,13 @@
 Методы api работы с Объектом FAQ
 """
 
-from fastapi import APIRouter, Body, Header, Request
+from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
 
 from app.api.responses import create_response, bad_request, not_found, get_token_in_header
 from app.api.schemas import Response_Base, Response_Q, Response_QAs, Response_QA_create, Response_QA_one, \
     QA_create, QA_update
-from app.models import FAQ, PyObjectId, User
+from app.models import FAQ, PyObjectId
 
 faq = APIRouter()
 
@@ -40,49 +40,34 @@ async def get_answer(_id: str):
 
 
 @faq.post('/create/', description='Создание новой записи вопрос + ответ', response_model=Response_QA_create)
-async def create(request: Request, payload: QA_create = Body(...)):
+async def create(payload: QA_create = Body(...)):
     """Создание нового объекта (вопрос+ответ)."""
 
-    user = await User.get_user(await get_token_in_header(request))
-    if await User.is_manager(user):
-        _id = await FAQ.create_qa(jsonable_encoder(payload), user)
-        if _id is not None:
-            return await create_response(data={'_id': _id}, message='New QA is created!')
-        msg = 'Failed to save'
-    else:
-        msg = 'Not access'
-    return await bad_request(msg)
+    _id = await FAQ.create_qa(jsonable_encoder(payload))
+    if _id is not None:
+        return await create_response(data={'_id': _id}, message='New QA is created!')
+    return await bad_request(message='Failed to save')
 
 
 @faq.put('/update/{_id: str}/', description='Редактирование существующей записи', response_model=Response_Base)
-async def update(request: Request, _id, payload: QA_update = Body(...)):
+async def update(_id, payload: QA_update = Body(...)):
     """Обновление существующего объекта по его id (обновление вопрос и/или ответ)."""
 
-    user = await User.get_user(await get_token_in_header(request))
-    if await User.is_manager(user):
-        flg = await FAQ.update_qa(_id, jsonable_encoder(payload))
-        if flg is None:
-            return await not_found(f'_id=[{_id}]')
-        elif flg:
-            return await create_response(message=f'Updated _id=[{_id}] is success')
-        msg = f'Updated _id=[{_id}] is failed'
-    else:
-        msg = 'Not access'
-    return await bad_request(msg)
+    flg = await FAQ.update_qa(_id, jsonable_encoder(payload))
+    if flg is None:
+        return await not_found(f'_id=[{_id}]')
+    elif flg:
+        return await create_response(message=f'Updated _id=[{_id}] is success')
+    return await bad_request(message=f'Updated _id=[{_id}] is failed')
 
 
 @faq.delete('/delete/{_id: str}/', description='Удаление записи', response_model=Response_Base)
-async def delete(request: Request, _id: PyObjectId):
+async def delete(_id: PyObjectId):
     """Удаление существующего объекта по его id."""
 
-    user = await User.get_user(await get_token_in_header(request))
-    if await User.is_manager(user):
-        flg = await FAQ.delete_qa(_id)
-        if flg is None:
-            return await not_found(f'_id=[{_id}]')
-        elif flg:
-            return await create_response(message=f'Deleted _id=[{_id}] is success')
-        msg = f'Deleted _id=[{_id}] is failed'
-    else:
-        msg = 'Not access'
-    return await bad_request(msg)
+    flg = await FAQ.delete_qa(_id)
+    if flg is None:
+        return await not_found(message=f'_id=[{_id}]')
+    elif flg:
+        return await create_response(message=f'Deleted _id=[{_id}] is success')
+    return await bad_request(message=f'Deleted _id=[{_id}] is failed')
