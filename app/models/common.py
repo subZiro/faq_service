5a6ff3e3bacb933f8fd77e34814b50b601ddff3c
Ilya <49365758+subZiro@users.noreply.github.com>
2022-06-07 18:22:29 +0400

    add common for models

---
+++ app/models/common.py
@@ -0,0 +1,31 @@
+from bson.objectid import ObjectId
+from pydantic import BaseModel
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class BaseDBModel(BaseModel):
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+
+        @classmethod
+        def alias_generator(cls, string: str) -> str:
+            """ Camel case generator """
+            temp = string.split('_')
+            return temp[0] + ''.join(ele.title() for ele in temp[1:])
+
