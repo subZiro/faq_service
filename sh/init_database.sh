2cf3fb04ae6668c2e868550e90346f2171f03ffe
Ilya <49365758+subZiro@users.noreply.github.com>
2022-06-07 18:09:25 +0400

    add script create db user

---
+++ sh/init_database.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+  var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+  var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+  var user = '$MONGO_INITDB_USERNAME';
+  var passwd = '$MONGO_INITDB_PASSWORD';
+
+  var admin = db.getSiblingDB('admin');
+
+  admin.auth(rootUser, rootPassword);
+  db.createUser({
+    user: user,
+    pwd: passwd,
+    roles: [
+      {
+        role: "root",
+        db: "admin"
+      }
+    ]
+  });
+EOF
