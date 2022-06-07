6c2093c844a0ffec21dc38aec5ebe47a60580561
Ilya <49365758+subZiro@users.noreply.github.com>
2022-06-07 18:11:20 +0400

    update application

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ init application.
 
 import os
 
-from app.api import home
+from app.api import home, faq
 from app.api.auth import authorization
 from app.common import create_application
 
@@ -12,3 +12,4 @@ print(os.getenv('FASTAPI_ENV'))
 api, conf = create_application(os.getenv('FASTAPI_ENV', 'default'))
 
 api.include_router(home, tags=["Main"], prefix='/home', dependencies=authorization)
+api.include_router(faq, tags=["FAQ"], prefix='/', dependencies=authorization)
