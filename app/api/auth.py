213733408dffb3f1962a98bbab3a72e09c7d40b2
Ilya <49365758+subZiro@users.noreply.github.com>
2022-06-07 17:57:31 +0400

    Авторизация к API

---
+++ app/api/auth.py
@@ -0,0 +1,25 @@
+"""
+Авторизация к API
+"""
+
+# from jose import jwt
+# from jose.exceptions import JOSEError
+
+from fastapi import Depends
+from fastapi.security import HTTPBasicCredentials, HTTPBearer
+
+from app.api.responses import unauthorized
+from app.models import User
+
+security = HTTPBearer()
+
+
+async def has_access(credentials: HTTPBasicCredentials = Depends(security)):
+    """Function that is used to validate the token in the case that it requires it"""
+    from app import conf
+    if credentials.credentials != conf.API_TOKEN:
+        msg = 'Не верное значение токена'
+        return await unauthorized(msg)
+
+
+authorization = [Depends(has_access)]
